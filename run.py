0d58619f01d53acf4b9265234840db2eb6769eae
cocofeyou@yunify.com <cocofeyou@yunify.com>
2020-01-10 22:25:10 +0800

    CountSquareSubmatriceswithAllOnes

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-from DynamicProgramming.WordBreak import Solution
+from DynamicProgramming.CountSquareSubmatriceswithAllOnes import Solution
 
 if __name__ == '__main__':
     s = Solution()
